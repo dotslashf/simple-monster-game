48f9c18d623a2b766f61bccea7fc7385df5e4456
fadhlu <fadhlurahman.i@gmail.com>
2020-03-21 00:49:45 +0700

    add more methods

--- js/app.js
+++ js/app.js
@@ -11,27 +11,67 @@ new Vue({
 			this.heroHealth = 100;
 			this.monsterHealth = 100;
 		},
-		calculteDamage: function(min, max) {
+		calculateDmg: function(min, max) {
+			return Math.max(Math.floor(Math.random() * max) + 1, min);
+		},
+		calculateHeal: function(min, max) {
 			return Math.max(Math.floor(Math.random() * max) + 1, min);
 		},
 		attack: function() {
-			var damage = this.calculteDamage(5, 15);
+			var damage = this.calculateDmg(5, 10);
 			this.monsterHealth -= damage;
 
+			var monsterDamage = this.calculateDmg(3, 12);
+			this.heroHealth -= monsterDamage;
+
+			this.checkWinner();
+		},
+		specialAttack: function() {
+			var damage = this.calculateDmg(10, 15);
+			this.monsterHealth -= damage;
+
+			var monsterDamage = this.calculateDmg(3, 9);
+			this.heroHealth -= monsterDamage;
+
+			this.checkWinner();
+		},
+		heal: function() {
+			if (this.heroHealth < 100) {
+				var heal = this.calculateHeal(8, 17);
+				this.heroHealth += heal;
+				if (this.heroHealth > 100) {
+					temp = this.heroHealth - 100;
+					this.heroHealth = this.heroHealth - temp;
+				}
+			} else {
+				alert('You Have Maximum Health!');
+				return;
+			}
+
+			var monsterDamage = this.calculateDmg(3, 12);
+			this.heroHealth -= monsterDamage;
+
+			this.checkWinner();
+		},
+		giveUp: function() {
+			var dialogBox = confirm('Are You Sure Want To Give Up?');
+			if (dialogBox) {
+				this.isRunning = false;
+				this.monsterHealth = 100;
+				this.heroHealth = 100;
+			}
+		},
+		checkWinner: function() {
 			if (this.monsterHealth <= 0) {
 				this.monsterHealth = 0;
 				alert('You Won!');
 				this.isRunning = false;
 				return;
-			}
-
-			var monsterDamage = this.calculteDamage(7, 17);
-			this.heroHealth -= monsterDamage;
-
-			if (this.heroHealth <= 0) {
+			} else if (this.heroHealth <= 0) {
 				this.heroHealth = 0;
 				alert('You Lost!');
 				this.isRunning = false;
+				return;
 			}
 		}
 	}
