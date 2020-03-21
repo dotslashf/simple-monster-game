609587eda7e0ef43ca1dd76250edab9b26a354d4
fadhlu <fadhlurahman.i@gmail.com>
2020-03-21 15:32:17 +0700

    add mana

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ new Vue({
 	data: {
 		heroHealth: 100,
 		monsterHealth: 100,
+		heroMana: 5,
 		isRunning: false,
 		turns: []
 	},
@@ -11,6 +12,7 @@ new Vue({
 			this.isRunning = true;
 			this.heroHealth = 100;
 			this.monsterHealth = 100;
+			this.heroMana = 5;
 			this.turns = [];
 		},
 		calculateDmg: function(min, max) {
@@ -34,14 +36,19 @@ new Vue({
 			this.checkWinner();
 		},
 		specialAttack: function() {
-			var heroDamage = this.calculateDmg(10, 15);
-			this.monsterHealth -= heroDamage;
-			this.turns.unshift({
-				isPlayer: true,
-				text: 'Hero attack Monster for ' + heroDamage
-			});
-			if (this.checkWinner()) {
-				return;
+			if (this.heroMana > 0) {
+				var heroDamage = this.calculateDmg(10, 15);
+				this.monsterHealth -= heroDamage;
+				this.heroMana -= 1;
+				this.turns.unshift({
+					isPlayer: true,
+					text: 'Hero attack Monster for ' + heroDamage
+				});
+				if (this.checkWinner()) {
+					return;
+				}
+			} else {
+				return alert('You have no mana left');
 			}
 
 			this.monsterAttack(3, 9);
@@ -51,6 +58,9 @@ new Vue({
 			if (this.heroHealth < 100) {
 				var heal = this.calculateHeal(8, 17);
 				this.heroHealth += heal;
+				if (this.heroMana < 5) {
+					this.heroMana += Math.floor(Math.random() * 2);
+				}
 				this.turns.unshift({
 					isPlayer: true,
 					text: 'Hero heals for ' + heal
